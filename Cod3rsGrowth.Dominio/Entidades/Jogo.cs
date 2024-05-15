8ce15b590aa421a7f81c8ad194ff71e5be3ba31a
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2024-05-15 09:25:20 -0300

    entidade ResponsavelDoTeste foi deletada e private set foram alterados para set

--- Cod3rsGrowth.Dominio/Entidades/Jogo.cs
+++ Cod3rsGrowth.Dominio/Entidades/Jogo.cs
@@ -3,10 +3,9 @@ namespace Cod3rsGrowth.Dominio.Entidades
 {
     public class Jogo
     {
-        public int Id { get; private set; }
-        public string Nome { get; private set; }
-        public Genero Genero { get; private set; }
-        public decimal Preco { get; private set; }
-        
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Genero Genero { get; set; }
+        public decimal Preco { get; set; }
     }
 }
--- Cod3rsGrowth.Dominio/Entidades/TesteDeJogo.cs
+++ Cod3rsGrowth.Dominio/Entidades/TesteDeJogo.cs
@@ -8,11 +8,11 @@ namespace Cod3rsGrowth.Dominio.Entidades
     public class TesteDeJogo
     {
         public int Id { get; private set; }
-        public ResponsavelDoTeste ResponsavelDoTeste { get; private set; }
-        public string Descricao { get; private set; }
-        public decimal Nota { get; private set; }
-        public bool Aprovado { get; private set; }
-        public DateTime DataRealizacaoTeste { get; private set; }
-        public Jogo JogoId { get; private set; }
+        public string NomeResponsavelDoTeste { get; set; }
+        public string Descricao { get; set; }
+        public decimal Nota { get; set; }
+        public bool Aprovado { get; set; }
+        public DateTime DataRealizacaoTeste { get; set; }
+        public Jogo JogoId { get; set; }
     }
 }
