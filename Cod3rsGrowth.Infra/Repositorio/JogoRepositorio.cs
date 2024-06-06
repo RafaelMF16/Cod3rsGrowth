2bc5224627322818c2ce30e0fc0c5881d544b13e
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2024-06-06 12:15:41 -0300

    os repositorios do banco de dados foram criados e o app config

---
+++ Cod3rsGrowth.Infra/Repositorio/JogoRepositorio.cs
@@ -0,0 +1,34 @@
+﻿using Cod3rsGrowth.Dominio.Entidades;
+using Cod3rsGrowth.Infra.Interfaces;
+using System.Collections.Generic;
+
+namespace Cod3rsGrowth.Infra.Repositorio
+{
+    public class JogoRepositorio : IJogoRepositorio
+    {
+        public void Adicionar(Jogo jogo)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Atualizar(Jogo jogo)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Deletar(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Jogo ObterPorId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Jogo> ObterTodos()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
