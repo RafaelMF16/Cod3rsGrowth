332c523874df964319da73d14c630c9b7e889ea6
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2024-06-21 10:45:13 -0300

    mudei o nome da janela do forms

--- Cod3rsGrowth.Forms/Program.cs
+++ Cod3rsGrowth.Forms/Program.cs
@@ -2,18 +2,13 @@ namespace Cod3rsGrowth.Forms
 {
     public static class Program 
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             var migracao = new Migracao();
             migracao.ExecutarMigracao();
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new FormsListaJogo());
         }
     }
 }
