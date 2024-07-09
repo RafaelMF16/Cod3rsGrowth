a29a6893e841fb94ca9ad1e488faafd7a92f2ff6
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2024-07-09 14:07:00 -0300

    Configurei a API com injeção de dependencias e criei os controllers

---
+++ Cod3rsGrowth.Web/Controllers/JogoControlador.cs
@@ -0,0 +1,48 @@
+﻿using Cod3rsGrowth.Servico.Servicos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cod3rsGrowth.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JogoControlador : ControllerBase
+    {
+        private readonly ServicoJogo _servicoJogo;
+
+        public JogoControlador(ServicoJogo servicoJogo)
+        {
+            _servicoJogo = servicoJogo
+                ?? throw new Exception($"Erro ao obter o serviço {typeof(ServicoJogo)}");
+        }
+
+        [HttpGet]
+        public IActionResult ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IActionResult Criar()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPatch]
+        public IActionResult Editar()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Apagar(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Cod3rsGrowth.Web/Injecao/ModuloDeInjecaoApi.cs
@@ -0,0 +1,43 @@
+﻿using Cod3rsGrowth.Dominio.Entidades;
+using Cod3rsGrowth.Infra.Interfaces;
+using Cod3rsGrowth.Infra.Repositorio;
+using Cod3rsGrowth.Infra;
+using Cod3rsGrowth.Servico.Servicos;
+using Cod3rsGrowth.Servico.Validadores;
+using FluentValidation;
+using LinqToDB.AspNet;
+using LinqToDB;
+using Cod3rsGrowth.Dominio.Migracao;
+using FluentMigrator.Runner;
+
+namespace Cod3rsGrowth.Web.Injecao
+{
+    public static class ModuloDeInjecaoApi
+    {
+        public static void AdicionarServicosAoEscopo(this WebApplicationBuilder builder)
+        {
+            const string nomeVariavelAmbiente = "ConnectionString";
+            var stringConexao = Environment.GetEnvironmentVariable(nomeVariavelAmbiente)
+                ?? throw new Exception($"Variavel de ambiente [{nomeVariavelAmbiente}] não encontrada");
+
+            builder.Services.AddLinqToDBContext<DbCod3rsGrowth>((provider, options)
+                => options
+                .UseSqlServer(stringConexao));
+
+            builder.Services.AddScoped<ServicoJogo>();
+            builder.Services.AddScoped<ServicoTesteDeJogo>();
+            builder.Services.AddScoped<IValidator<Jogo>, JogoValidador>();
+            builder.Services.AddScoped<IValidator<TesteDeJogo>, TesteDeJogoValidador>();
+            builder.Services.AddScoped<IJogoRepositorio, JogoRepositorio>();
+            builder.Services.AddScoped<ITesteDeJogoRepositorio, TesteDeJogoRepositorio>();
+
+            builder.Services.AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                .AddSqlServer()
+                .WithGlobalConnectionString(stringConexao)
+                .ScanIn(typeof(_20240620104300_migracao_tabela_jogo).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+    }
+}
