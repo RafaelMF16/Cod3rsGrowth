6cd16a0bdc26449d8adfa108902abf241907fd3e
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2024-07-11 09:43:11 -0300

    Corrigi os nomes em inglês

--- Cod3rsGrowth.Web/ExtensaoDosDetalhesDeErro.cs
+++ Cod3rsGrowth.Web/ExtensaoDosDetalhesDeErro.cs
@@ -13,46 +13,46 @@ public static class ExtensaoDosDetalhesDeErro
         {
             builder.Run(async context =>
             {
-                var TratadorDeExcecoes = context.Features.Get<IExceptionHandlerFeature>();
-                if (TratadorDeExcecoes != null)
+                var tratadorDeExcecoes = context.Features.Get<IExceptionHandlerFeature>();
+                if (tratadorDeExcecoes != null)
                 {
-                    var exception = TratadorDeExcecoes.Error;
-                    var problemDetails = new ProblemDetails
+                    var excecao = tratadorDeExcecoes.Error;
+                    var detalhesDeErro = new ProblemDetails
                     {
                         Instance = context.Request.HttpContext.Request.Path
                     };
 
-                    if (exception is ValidationException validationException)
+                    if (excecao is ValidationException validationException)
                     {
-                        problemDetails.Title = "Erro de validação";
-                        problemDetails.Status = StatusCodes.Status400BadRequest;
-                        problemDetails.Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1";
-                        problemDetails.Detail = validationException.StackTrace;
-                        problemDetails.Extensions["Erros de validação"] = validationException.Errors
+                        detalhesDeErro.Title = "Erro de validação";
+                        detalhesDeErro.Status = StatusCodes.Status400BadRequest;
+                        detalhesDeErro.Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1";
+                        detalhesDeErro.Detail = validationException.StackTrace;
+                        detalhesDeErro.Extensions["Erros de validação"] = validationException.Errors
                         .GroupBy(nomePropriedade => nomePropriedade.PropertyName, mensagemErro => mensagemErro.ErrorMessage)
                         .ToDictionary(x => x.Key, x => x.ToList());
                     }
-                    else if (exception is SqlException sqlException)
+                    else if (excecao is SqlException sqlException)
                     {
-                        problemDetails.Title = "Erro no banco de dados";
-                        problemDetails.Status = StatusCodes.Status500InternalServerError;
-                        problemDetails.Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1";
-                        problemDetails.Detail = sqlException.StackTrace;
-                        problemDetails.Extensions["Erro no banco de dados"] = sqlException.Message;
+                        detalhesDeErro.Title = "Erro no banco de dados";
+                        detalhesDeErro.Status = StatusCodes.Status500InternalServerError;
+                        detalhesDeErro.Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1";
+                        detalhesDeErro.Detail = sqlException.StackTrace;
+                        detalhesDeErro.Extensions["Erro no banco de dados"] = sqlException.Message;
                     }
                     else
                     {
                         var logger = loggerFactory.CreateLogger("GlobalExceptionHandler");
-                        logger.LogError($"Erro inesperado: {TratadorDeExcecoes.Error}");
-                        problemDetails.Title = "Erro inesperado";
-                        problemDetails.Status = StatusCodes.Status500InternalServerError;
-                        problemDetails.Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1";
-                        problemDetails.Detail = exception.Demystify().ToString();
-                        problemDetails.Extensions["Erro inesperado"] = exception.Message;
+                        logger.LogError($"Erro inesperado: {tratadorDeExcecoes.Error}");
+                        detalhesDeErro.Title = "Erro inesperado";
+                        detalhesDeErro.Status = StatusCodes.Status500InternalServerError;
+                        detalhesDeErro.Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1";
+                        detalhesDeErro.Detail = excecao.Demystify().ToString();
+                        detalhesDeErro.Extensions["Erro inesperado"] = excecao.Message;
                     }
-                    context.Response.StatusCode = problemDetails.Status.Value;
+                    context.Response.StatusCode = detalhesDeErro.Status.Value;
                     context.Response.ContentType = "application/problem+json";
-                    var json = JsonConvert.SerializeObject(problemDetails);
+                    var json = JsonConvert.SerializeObject(detalhesDeErro);
                     await context.Response.WriteAsync(json);
                 }
             });
