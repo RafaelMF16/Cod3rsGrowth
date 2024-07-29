0b6894db108c525d93aa96bf1197417e1e47b12a
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2024-07-29 13:13:27 -0300

    Implementei um botão que volta para a página inicial

--- Cod3rsGrowth.Web/wwwroot/controller/PaginaNaoEncontrada.controller.js
+++ Cod3rsGrowth.Web/wwwroot/controller/PaginaNaoEncontrada.controller.js
@@ -1,9 +1,13 @@
 sap.ui.define([
     "ui5/codersgrowth/controller/BaseController"
- ], function (BaseController) {
+], function (BaseController) {
     "use strict";
     return BaseController.extend("ui5.codersgrowth.controller.PaginaNaoEncontrada", {
-       onInit: function () {
-       }
+        onInit: function () {
+        },
+
+        voltarParaPaginaInicial: function () {
+            this.getRouter().navTo("appJogo", {}, true);
+        }
     });
- });
+});
