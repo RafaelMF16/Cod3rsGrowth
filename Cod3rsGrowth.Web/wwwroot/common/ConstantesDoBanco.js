821a498a0cbc345204db621e388a3d6af0b68cdb
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2024-09-23 19:31:57 -0300

    A tela de adição foi refatorada

---
+++ Cod3rsGrowth.Web/wwwroot/common/ConstantesDoBanco.js
@@ -0,0 +1,12 @@
+sap.ui.define([],
+    function() {
+        'use strict';
+    
+        const constantesDoBanco = {
+            CAMINHO_PARA_API_JOGO: "/api/JogoControlador",
+            CAMINHO_PARA_API_AVALIACAO: "/api/TesteDeJogoControlador",
+            CAMINHO_PARA_API_GENERO: "/api/GeneroControlador"
+        };
+    
+        return constantesDoBanco;
+});
